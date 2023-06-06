413aa79f3e9a97b8ca1008764545ef21f4de9799
Mert Erciyas <99058235@mydavinci.nl>
2023-06-06 14:43:44 +0200

    assignment 2

--- Assignment 1/Pokemon Battle/Pokemon Battle/Program.cs
+++ Assignment 1/Pokemon Battle/Pokemon Battle/Program.cs
@@ -1,67 +1,20 @@
 ﻿using System.Xml.Linq;
 using System.Collections.Generic;
-
-class Charmander
+namespace Pokemon_Battle_Simulator
 {
-    public string Name { get; set; }
-    public string Strenght { get; set; }
-    public string Weakness { get; set; }
-
-    public Charmander(string name,string strenght, string weakness) 
+    class Program
     {
-        Name = name;
-        Strenght = strenght;
-        Weakness = weakness;
-
-    }
-
-    public string GetName()
-    {
-        return Name;
-    }
-
-    public void SetName(string NewName)
-    {
-        Name = NewName;
-    }
-
-    public string GetStrenght()
-    {
-        return Strenght;
-    }
-
-    public void SetStrenght(string NewStrenght)
-    {
-        Strenght = NewStrenght;
-    }
-    public string GetWeakness()
-    {
-        return Weakness;
-    }
-
-    public void SetWeakness(string NewWeakness)
-    {
-        Weakness = NewWeakness;
-    }
-
-    public void Battlecry() 
-    {
-        Console.WriteLine($"Your pokemon calls out his name: {Name}!");
-    }    
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        while (true)
+        static void Main(string[] args)
         {
-            Console.Write("What would you like to call the Charmander? : ");
-            string Charmander_Name = Console.ReadLine();
-            Charmander charmander1 = new Charmander(Charmander_Name, "Fire", "Water");
-            for (int i = 0; i < 10; i++)
+            while (true)
             {
-                Console.WriteLine(charmander1.GetName());
+                Console.Write("What would you like to call the Charmander? : ");
+                string Charmander_Name = Console.ReadLine();
+                Charmander charmander1 = new Charmander(Charmander_Name, "Fire", "Water");
+                for (int i = 0; i < 1; i++)
+                {
+                    Console.WriteLine(charmander1.GetName());
+                }
             }
         }
     }
